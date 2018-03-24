7039c1cf8ffa844f3358447cb6a312cbd2e2894f
hepiska franatagola <ego1403@gmail.com>
2018-03-24 15:05:48 +0700

    lalalla

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,23 @@
 <template>
-    <v-layout align-center justify-center>
-    <v-flex xs12 sm4 md4 text-xs-center mt-5>
-      <h3>ini halaman profile</h3>
+    <v-layout align-center justify-center pa-0>
+    <v-flex xs12 sm4 md4 text-xs-center pa-0>
+      <v-layout justify-center pb-4>
+        <v-flex xs3 justify-center>
+          <v-avatar
+            size="60px"
+            class="grey lighten-4"
+           >
+          <img src="https://avatars1.githubusercontent.com/u/17113995?s=400&v=4" alt="avatar">
+        </v-avatar>
+       </v-flex>
+       <v-flex xs9 justify-center> 
+          foto
+       </v-flex> 
+     </v-layout> 
+     <v-divider></v-divider>
+      <v-layout pt-4>
+        profile header
+     </v-layout> 
     </v-flex>
   </v-layout>
 </template>
--- pages/submit-loan.vue
+++ pages/submit-loan.vue
@@ -1,34 +1,36 @@
 <template>
-    <v-layout align-center justify-center>
-    <v-flex xs12 sm4 md4 text-xs-center mt-5>
-      <v-stepper v-model="e1">
-        <v-stepper-header>
-        <v-stepper-step step="1" :complete="e1 > 1">Name of step 1</v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="2" :complete="e1 > 2">Name of step 2</v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="3">Name of step 3</v-stepper-step>
-        </v-stepper-header>
-        <v-stepper-items>
-        <v-stepper-content step="1">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
-            <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
-        <v-stepper-content step="2">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
-            <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
-        <v-stepper-content step="3">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
-            <v-btn flat>Cancel</v-btn>
-        </v-stepper-content>
-        </v-stepper-items>
-    </v-stepper>
-    </v-flex>
-  </v-layout>
+  <v-layout align-center justify-center>
+  <v-flex xs12 sm6 md6 text-xs-center mt-5>
+    <v-stepper v-model="step" alt-labels>
+      <v-stepper-header>
+      <v-stepper-step step="1" :complete="step > 1">Data Rumah</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="2" :complete="step > 2">Name of step 2</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3">Name of step 3</v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-items>
+      <v-stepper-content step="1">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"><v-flex></v-flex>
+          </v-card>
+          <v-btn color="primary" @click.native="step = 2">Continue</v-btn>
+          <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="2">
+          <v-card color="grey lighten-1" class="mb-5" height="200px">
+          </v-card>
+          <v-btn color="primary" @click.native="step = 3">Continue</v-btn>
+          <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="3">
+          <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click.native="step = 1">Continue</v-btn>
+          <v-btn flat>Cancel</v-btn>
+      </v-stepper-content>
+      </v-stepper-items>
+  </v-stepper>
+  </v-flex>
+</v-layout>
 </template>
 <script>
 import bahasa from 'vee-validate/dist/locale/id'
@@ -38,7 +40,18 @@ export default {
   },
     data () {
         return {
-            e1: 0
+            step: 0,
+            valid: false,
+            name: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => v.length <= 10 || 'Name must be less than 10 characters'
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ]
         }
     },
     mounted() {
